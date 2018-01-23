e6a627e8c8c75c72203f2fe16f6a9e00c23fee1d
piyush97 <me@piyushmehta.com>
2018-01-23 10:41:06 +0530

    Oncreate Method modified for website

--- app/src/main/java/com/piyushmehta/mycollegeforums/MainActivity.java
+++ app/src/main/java/com/piyushmehta/mycollegeforums/MainActivity.java
@@ -2,12 +2,17 @@ package com.piyushmehta.mycollegeforums;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
+private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.web);
+        WebSettings webSettings=webView.getSettings();
     }
 }

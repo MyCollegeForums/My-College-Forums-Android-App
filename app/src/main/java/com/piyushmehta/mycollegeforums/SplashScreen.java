d07784bc018247f4b2d126fd66542d35127097bf
piyush97 <me@piyushmehta.com>
2018-01-23 12:16:40 +0530

    Gradle Updated
    Api level increased, Splash Screen Added, Full Screen Added

--- app/src/main/java/com/piyushmehta/mycollegeforums/SplashScreen.java
+++ app/src/main/java/com/piyushmehta/mycollegeforums/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.piyushmehta.mycollegeforums;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends Activity {
@@ -9,5 +10,20 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread myThread =new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }

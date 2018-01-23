ff9a817754ff18a6b8a82ca29b703bf06fa02d3b
piyush97 <me@piyushmehta.com>
2018-01-23 21:06:49 +0530

    Hardware and Performance Improvements :100:

--- app/src/main/java/com/piyushmehta/mycollegeforums/MainActivity.java
+++ app/src/main/java/com/piyushmehta/mycollegeforums/MainActivity.java
@@ -2,38 +2,142 @@ package com.piyushmehta.mycollegeforums;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends Activity {
 private WebView webView;
     private static final String TAG = "MainActivity";
-
+    private FirebaseAuth mAuth;
     private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
         webView=findViewById(R.id.web);
         mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
+        mAuth = FirebaseAuth.getInstance();
         mAdView.loadAd(adRequest);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webView.getSettings().setSavePassword(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setEnableSmoothTransition(true);
+
+
         WebSettings webSettings=webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        webView.loadUrl("https://mycollegeforums.com");
         webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://mycollegeforums.com");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }/*
+    public void onClick(View view) {
+        if (view.getId() == R.id.defaultTheme) {
+            new FinestWebView.Builder(this).titleDefault("The Finest Artist")
+                    .show("http://thefinestartist.com");
+            ////                    .toolbarScrollFlags(0)
+            //                    .webViewJavaScriptEnabled(true)
+            //                    .webViewUseWideViewPort(false)
+            ////                    .show("http://andrewliu.in/2016/01/30/聊聊Redis的订阅发布/");
+            //                    .show("http://www.youtube.com");
+        } else if (view.getId() == R.id.redTheme) {
+            //            Intent intent = new Intent(this, WebViewActivity.class);
+            //            startActivity(intent);
+            new FinestWebView.Builder(this).theme(R.style.RedTheme)
+                    .titleDefault("Bless This Stuff")
+                    .webViewBuiltInZoomControls(true)
+                    .webViewDisplayZoomControls(true)
+                    .dividerHeight(0)
+                    .gradientDivider(false)
+                    .setCustomAnimations(R.anim.activity_open_enter, R.anim.activity_open_exit,
+                            R.anim.activity_close_enter, R.anim.activity_close_exit)
+                    .injectJavaScript("javascript: document.getElementById('msg').innerHTML='Hello "
+                            + "TheFinestArtist"
+                            + "!';")
+                    .show("http://www.blessthisstuff.com");
+        } else if (view.getId() == R.id.blueTheme) {
+            new FinestWebView.Builder(this).theme(R.style.FinestWebViewTheme)
+                    .titleDefault("Vimeo")
+                    .showUrl(false)
+                    .statusBarColorRes(R.color.bluePrimaryDark)
+                    .toolbarColorRes(R.color.bluePrimary)
+                    .titleColorRes(R.color.finestWhite)
+                    .urlColorRes(R.color.bluePrimaryLight)
+                    .iconDefaultColorRes(R.color.finestWhite)
+                    .progressBarColorRes(R.color.finestWhite)
+                    .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+                    .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+                    .stringResCopiedToClipboard(R.string.copied_to_clipboard)
+                    .showSwipeRefreshLayout(true)
+                    .swipeRefreshColorRes(R.color.bluePrimaryDark)
+                    .menuSelector(R.drawable.selector_light_theme)
+                    .menuTextGravity(Gravity.CENTER)
+                    .menuTextPaddingRightRes(R.dimen.defaultMenuTextPaddingLeft)
+                    .dividerHeight(0)
+                    .gradientDivider(false)
+                    .setCustomAnimations(R.anim.slide_up, R.anim.hold, R.anim.hold, R.anim.slide_down)
+                    .show("http://example.com");
+        } else if (view.getId() == R.id.blackTheme) {
+            new FinestWebView.Builder(this).theme(R.style.FinestWebViewTheme)
+                    .titleDefault("Dribbble")
+                    .toolbarScrollFlags(0)
+                    .statusBarColorRes(R.color.blackPrimaryDark)
+                    .toolbarColorRes(R.color.blackPrimary)
+                    .titleColorRes(R.color.finestWhite)
+                    .urlColorRes(R.color.blackPrimaryLight)
+                    .iconDefaultColorRes(R.color.finestWhite)
+                    .progressBarColorRes(R.color.finestWhite)
+                    .swipeRefreshColorRes(R.color.blackPrimaryDark)
+                    .menuSelector(R.drawable.selector_light_theme)
+                    .menuTextGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT)
+                    .menuTextPaddingRightRes(R.dimen.defaultMenuTextPaddingLeft)
+                    .dividerHeight(0)
+                    .gradientDivider(false)
+                    //                    .setCustomAnimations(R.anim.slide_up, R.anim.hold, R.anim.hold, R.anim.slide_down)
+                    .setCustomAnimations(R.anim.slide_left_in, R.anim.hold, R.anim.hold,
+                            R.anim.slide_right_out)
+                    //                    .setCustomAnimations(R.anim.fade_in_fast, R.anim.fade_out_medium, R.anim.fade_in_medium, R.anim.fade_out_fast)
+                    .disableIconBack(true)
+                    .disableIconClose(true)
+                    .disableIconForward(true)
+                    .disableIconMenu(true)
+                    .show("https://mycollegeforums.com");
+        }
     }
 
-
-    @Override
+*/
+  @Override
     public void onBackPressed() {
         if(webView.canGoBack())
         {
@@ -45,6 +149,5 @@ private WebView webView;
 
         }
     }
-
 
 }
